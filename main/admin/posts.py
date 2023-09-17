137b0b9a69ea0cdf76060967971f5038fa58f3f9
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2023-09-17 12:12:11 +0500

    navbar and posts models added

--- main/admin/posts.py
+++ main/admin/posts.py
@@ -1,2 +1,127 @@
-from mptt.admin import MPTTModelAdmin # for navbar
+import random
+from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from main.models.posts import Navbar, Posts
 from guardian.admin import GuardedModelAdmin
+from django.utils.safestring import mark_safe
+from django.utils.translation import gettext_lazy as _
+
+
+# django admin actions
+@admin.action(description="clone post")
+def duplicate(modeladmin, request, queryset):
+    for i in queryset:
+        i.pk = None
+        i.slug += str(random.randint(9999, 9999))
+        i.save()
+
+
+@admin.register(Navbar)
+class NavbarAdmin(MPTTModelAdmin):
+    """ Admin view for Navigation bar model """
+    ordering = ("name", )
+    search_fields = ("name", )
+    date_hierarchy = "added_at"
+    list_display_links = ("name", )
+    list_filter = ("parent", "added_at", "visible", "author")
+    list_editable = ("order_num", "inside_order_num", "visible")
+    readonly_fields = ("author", "added_at", "update_user", "updated_at")
+    list_display = ("id", "name", "parent", "order_num", "inside_order_num", "visible", "added_at")
+
+    fieldsets = (
+        (_("Nomi"), {
+            "classes": ("extrapretty"),
+            "fields": (
+                "name",             
+            ),
+        }),
+        (_("Umumiy o'zgaruvchilar"), {
+            "classes": ("extrapretty"),
+            "fields": (
+                "parent", 
+                "order_num", 
+                "inside_order_num", 
+                "visible",                
+            ),
+        }),
+        (_("Automatik to'ldiriladigan fieldlar"), {
+            "classes": ("collapse", ),
+            "fields": (
+                "author",
+                "slug",
+                "added_at",
+                "update_user",
+                "updated_at"
+            ),
+        }),
+    )
+
+    def save_model(self, request, obj, form, change):
+        if obj.author:
+            obj.update_user = request.user
+        else:
+            obj.auhtor = request.user
+        return super().save_model(request, obj, form, change)
+    
+    def get_prepopulated_fields(self, request, obj):
+        return {"slug": ("name", )}
+
+
+
+@admin.register(Posts)
+class PostsAdmin(GuardedModelAdmin):
+    """ Admin view for Posts """
+    ordering = ("-added_at", )
+    actions = (duplicate, )
+    date_hierarchy = "added_at"
+    empty_value_display = "Tanlanmagan"
+    search_fields = ("title", "navbar__name")
+    list_filter = ("navbar", "status", "author", "added_at")
+    readonly_fields = ("author", "update_user", "updated_at", "get_image_file")
+    list_display = ("id", "title", "navbar", "status", "post_viewed_count", "author", "added_at", "updated_at")
+
+    fieldsets = (
+        (_("Umumiy o'zgaruvchilar"), {
+            "classes": ("extrapretty"),
+            "fields": (
+                "navbar", "status",
+                "added_at", "author_post",                 
+            ),
+        }),
+        (_("Media fayllar"), {
+            "fields": (
+                ("image", "get_image_file"),
+                ("pdf_file", "video_file")                
+            ),
+        }),
+        (_("O'zbek tilida"), {
+            "classes": ("extrapretty"),
+            "fields": (
+                "title", "subtitle", "post"                
+            ),
+        }),
+        (_("Automatik to'ldiriladigan fieldlar"), {
+            "classes": ("collapse", ),
+            "fields": (
+                "author",
+                "slug",
+                "post_viewed_count",
+                "update_user",
+                "updated_at"
+            ),
+        }),
+    )
+
+    def get_image_file(self, obj):
+        return mark_safe(f"<img src='{obj.image.url}' alt='asosiy rasm' width=200/>")
+    get_image_file.short_description = _("Post uchun tanlangan rasmli fayl")
+
+    def save_model(self, request, obj, form, change):
+        if obj.author:
+            obj.update_user = request.user
+        else:
+            obj.auhtor = request.user
+        return super().save_model(request, obj, form, change)
+    
+    def get_prepopulated_fields(self, request, obj):
+        return {"slug": ("title", )}
---
+++ main/models/posts.py
@@ -0,0 +1,82 @@
+from django.db import models
+from django_quill.fields import QuillField
+from django.contrib.auth.models import User
+from mptt.models import TreeForeignKey, MPTTModel
+from django.utils.translation import gettext_lazy as _
+
+
+
+class Navbar(MPTTModel):
+    """ Navigation bar model """
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, 
+        verbose_name=_("object_make_user"), related_name="navbar_author"
+    )
+    name = models.CharField(verbose_name=_("Nomi"), max_length=100)
+    parent = TreeForeignKey("self", related_name="children", on_delete=models.SET_NULL, null=True, blank=True)
+    visible = models.BooleanField(default=True, verbose_name=_("Ko'rinish"))
+    order_num = models.IntegerField(verbose_name=_("Tartib raqami"), default=0)
+    inside_order_num = models.IntegerField(default=0, verbose_name=_("Ichki tartib raqam"))
+    slug = models.SlugField(max_length=120, unique=True)
+    added_at = models.DateTimeField(auto_now_add=True)
+    update_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="update_navbar_user")
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class MPTTMeta:
+        order_insertion_by = ("inside_order_num", )
+        added_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        db_table = "navbar"
+        managed = True
+        verbose_name = _("Bo'limlar")
+        verbose_name_plural = _("Bo'limlar")
+
+
+    def __str__(self):
+        return str(self.name) if self.name else None
+
+
+class Status(models.TextChoices):
+    published: str = "pub", _("Published")
+    pendding: str = "pen", _("Pendding")
+
+
+class Posts(models.Model):
+    """ Posts """
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, 
+        verbose_name=_("object_make_user"), related_name="post_author"
+    )
+    navbar = TreeForeignKey(to=Navbar, on_delete=models.SET_NULL, null=True, verbose_name=_("Bo'lim nomi"))
+    image = models.ImageField(verbose_name=_("Post uchun asosiy rasm"), upload_to="posts/images/%Y-%m-%d/", null=True)
+    title = models.CharField(verbose_name=_("Sarlovha"), max_length=400, null=True)
+    subtitle = models.CharField(verbose_name=_("Qisqacha mazmun"), max_length=500, null=True)
+    post = QuillField(null=True)
+    pdf_file = models.FileField(
+        verbose_name=_("PDF fayl"), upload_to="posts/pdf-files/%Y-%m-%d/", 
+        null=True, blank=True, help_text=_("Faqat *.pdf formatdagi faylarni yuklang")
+    )
+    video_file = models.FileField(
+        verbose_name=_("Video fayl"), upload_to="posts/videos/%Y-%m-%d/",
+        null=True, blank=True, help_text=_("agar video fayl mavjud bo'lsa yuklang.")
+    )
+    status = models.CharField(verbose_name=_("post status"), max_length=50, choices=Status.choices, default=Status.pendding)
+    slug = models.SlugField(max_length=255, verbose_name="slug", help_text=_("Majburyat tug'ulmasa tegmang"))
+    post_viewed_count = models.IntegerField(default=0, verbose_name=_("Ko'rilganlik soni"), help_text=_("Tegilmasin !"))
+    author_post = models.CharField(verbose_name=_("Post muallifi"), max_length=300, default="TKTI axborot xizmati")
+    added_at = models.DateTimeField(verbose_name=_("Vaqt & sana"))
+    update_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="update_post_user")
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "posts"
+        managed = True
+        verbose_name = "Postlar"
+        verbose_name_plural = "Postlar"
+
+    def __str__(self):
+        return str(self.title) if self.title else None
+    
+    
+    
