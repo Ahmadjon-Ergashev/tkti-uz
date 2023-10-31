cbe1241df2921258b0432e42d91b4d193bc50287
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2023-10-31 17:05:28 +0500

    fixed base_dir

--- config/settings/base.py
+++ config/settings/base.py
@@ -16,7 +16,8 @@ from pathlib import Path
 env = environ.Env() # for .env.dev
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 environ.Env.read_env(os.path.join(BASE_DIR, '.env.dev')) # for .env.dev
  
 
