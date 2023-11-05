c41f13cb37f291bee310534a2b05126c6f527a8d
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2023-11-05 20:13:44 +0500

    x

--- config/settings/prod_settings.py
+++ config/settings/prod_settings.py
@@ -4,6 +4,8 @@ from config.settings.base import *
 ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS").split(" ")
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 CSP_FRAME_ANCESTORS = "https://tkti.toshvil.uz"
+X_FRAME_OPTIONS = 'ALLOW-FROM https://tkti.toshvil.uz/media/'
+
 DATABASES = {
     'default': {
         'ENGINE': env("POSTGRES_ENGINE"),
