95527433d979af308f43f35e96745aeb24a557e8
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2023-11-05 14:50:08 +0500

    add reload option in restart.py

--- restart.py
+++ restart.py
@@ -9,6 +9,7 @@ parser = argparse.ArgumentParser(description=desc)
 def Main():
     parser.add_argument('-r', '--restart', type=str, required=False, help='restart docker project')
     parser.add_argument('-p', '--prune', type=str, required=False, help='prune all containers and images')
+    parser.add_argument('-rl', '--reload', type=str, required=False, help='reload nginx and gunicorn')
     args = parser.parse_args()
     if args.restart:
         subprocess.call(["sudo", "docker", "container", "restart", "tkti_web"])
@@ -20,7 +21,13 @@ def Main():
         print(volumes)
         volume_name = input("enter volume name: ")
         subprocess.call(["sudo", "docker", "volume", "rm", volume_name])
-        subprocess.call(["sudo", "docker", "compose", "up", "-d", "--build"])
+        # subprocess.call(["sudo", "docker", "compose", "up", "-d", "--build"])
+    elif args.reload:
+        subprocess.call(["sudo", "systemctl", "daemond-reload"])
+        subprocess.call(["sudo", "systemctl", "restart", "tkti"])
+        subprocess.call(["sudo", "nginx", "-t"])
+        subprocess.call(["sudo", "systemctl", "restart", "nginx"])
+        print("reload complited")
     else:
         subprocess.call(["python3", "restart.py", "--help"])
 Main()
