ac0ba4f3ffff19294a1411905a846ac938010be4
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2024-02-04 20:57:22 +0500

    redis set

--- config/settings/base.py
+++ config/settings/base.py
@@ -243,3 +243,17 @@ INTERNAL_IPS = [
 # JAZZMIN_UI_TWEAKS = {
 #     "theme": "darkly"
 # }
+
+# redis
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
