79212867d5af486eac0092402f7c13ccfac2e367
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2024-08-29 09:40:56 +0500

    fix

--- static/js/scripts/posts/department_administrations.js
+++ static/js/scripts/posts/department_administrations.js
@@ -1,16 +1,14 @@
 $(document).ready(function () {
-    $('.department-admininstration[data-position]').click(function () {
+    // Function to load data based on position and department
+    function loadAdministrationData(position, department) {
         let base_url = window.location.origin;
-        let position = $(this).data('position');
-        let department = $(this).data('department');
 
         $.ajax({
             type: "GET",
             url: base_url + "/api/department/administration/",
             data: {position: position, department: department},
             success: function (data) {
-
-                $("#administration_item").empty()
+                $("#administration_item").empty();
                 data.map((item) => {
                     let card_item = `
                         <tr>
@@ -19,10 +17,20 @@ $(document).ready(function () {
                             <td>${item.email}</td>
                             <td><a href="${item.cv}" target="_blank">${item.view}</a></td>
                         </tr>
-                    `
-                    $("#administration_item").append(card_item)
-                })
+                    `;
+                    $("#administration_item").append(card_item);
+                });
             }
         });
+    }
+
+    // Attach click event to elements
+    $('.department-admininstration[data-position]').click(function () {
+        let position = $(this).data('position');
+        let department = $(this).data('department');
+        loadAdministrationData(position, department);
     });
+
+    // Trigger click on the first department item to load default data
+    $('.department-admininstration[data-position]').first().trigger('click');
 });
