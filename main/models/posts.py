88e27fa208c243532b183f69a8d9a7356a49a310
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2024-10-09 21:10:24 +0500

    fux

--- main/models/posts.py
+++ main/models/posts.py
@@ -257,7 +257,7 @@ class UniversityAdministrationsImages(models.Model):
             im = im.convert('RGB')
             im = ImageOps.exif_transpose(im)
             im_io = BytesIO()
-            im.save(im_io, 'JPEG', quality=25)
+            im.save(im_io, 'JPEG', quality=50)
             filename = os.path.splitext(self.image.name)[0]
             filename = f"{filename}.jpg"
             new_image = File(im_io, name=filename)
