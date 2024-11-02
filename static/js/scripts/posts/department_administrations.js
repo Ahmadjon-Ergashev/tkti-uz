abf203e6a95ea23d3ada26fa698c933bc22e433d
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2024-11-02 10:50:24 +0500

    study plan page and department admistration page fix

--- static/js/scripts/posts/department_administrations.js
+++ static/js/scripts/posts/department_administrations.js
@@ -15,7 +15,27 @@ $(document).ready(function () {
                             <td>${item.f_name}</td>
                             <td>${item.position.name}</td>
                             <td>${item.email}</td>
-                            <td><a href="${item.cv}" target="_blank">${item.view}</a></td>
+                            <td>
+                                <a type="button" class="text-primary"
+                                   data-bs-toggle="offcanvas"
+                                   data-bs-target="#cv_${item.id}"
+                                   aria-controls="cv_${item.id}">
+                                    ${item.view}
+                                </a>
+                                <div class="offcanvas offcanvas-start w-100 w-75-m w-75-l" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1"
+                                     id="cv_${item.id}" aria-labelledby="offcanvasScrollingLabel">
+                                    <div class="offcanvas-header">
+                                        <h5 class="offcanvas-title" id="cv_${item.id}">${item.view}</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                    </div>
+                                    <div class="offcanvas-body">
+                                        <iframe
+                                                src="${item.cv}"
+                                                width="100%" height="100%">
+                                        </iframe>
+                                    </div>
+                                </div>
+                            </td>
                         </tr>
                     `;
                     $("#administration_item").append(card_item);
